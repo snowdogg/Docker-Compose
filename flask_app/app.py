9f6c3b85bb428576c443be79849bec51bfa2b5d3
Andreas Cary <andreascary@gmail.com>
2020-10-14 16:09:19 -0700

    attached to redis db

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,10 +2,12 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
+counter = redis.Redis(host='redis', port=6379)
 
 @app.route("/")
 def home():
-	return "This is a fantastic web app, and it is powered by Flask."
+	counter.incr('hits')
+	return "This is a fantastic web app, and it is powered by Flask. \nIt has been viewed %d time(s)." % int(counter.get('hits'))
 
 if __name__ == "__main__":
 	app.run(debug=False, host='0.0.0.0')
